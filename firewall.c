1c5a68c8cfab5629aba408aa260a12adc9613844
Byeori <99kbr11@gmail.com>
2023-04-03 03:35:11 -0700

    Add sample code

--- firewall.c
+++ firewall.c
@@ -3,3 +3,25 @@
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
 
+static struct nf_hook_ops nfho; // Netfilter hook structure
+
+// Callback function for Netfilter hook
+unsigned int my_hook_fn(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
+    // Implement firewall logic here
+    return NF_ACCEPT; // Allow packet by default
+}
+
+// Initialization function for module
+int init_module() {
+    nfho.hook = my_hook_fn;
+    nfho.pf = PF_INET; // IPv4 packets only
+    nfho.hooknum = NF_INET_PRE_ROUTING; // Hook before routing decision
+    nfho.priority = NF_IP_PRI_FIRST; // Highest priority
+    nf_register_hook(&nfho); // Register hook with Netfilter
+    return 0;
+}
+
+// Cleanup function for module
+void cleanup_module() {
+    nf_unregister_hook(&nfho); // Unregister hook from Netfilter
+}
